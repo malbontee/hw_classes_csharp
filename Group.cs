e7655b7e8551fbf6ad73d2f1d3cec0e5ca18672a
malbontee <144820547+malbontee@users.noreply.github.com>
2025-01-24 12:56:34 +0200

    Добавьте файлы проекта.

---
+++ Group.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using hw_classes_csharp;
+
+namespace hw_classes_csharp
+{
+    public class Group
+    {
+        private List<Student> students;
+        private string? groupName;
+        private string? groupSpecialization;
+        private int groupCourse;
+
+
+        public Group()
+            : this("Unknown", "Unknown", 1) { }
+
+        public Group(string groupName, string groupSpecialization, int groupCourse)
+        {
+            students = new List<Student>();
+            SetGroupInfo(groupName, groupSpecialization, groupCourse);
+        }
+        public void SetGroupInfo(string groupName, string groupSpecialization, int groupCourse)
+        {
+            this.groupName = groupName;
+            this.groupSpecialization = groupSpecialization;
+            if (groupCourse > 0 && groupCourse <= 6)
+                this.groupCourse = groupCourse;
+            else
+            {
+                Console.WriteLine("Invalid value for course!");
+                return;
+            }
+        }
+        public (string groupName, string groupSpecialization, int groupCourse) GetGroupInfo()
+        {
+            return (groupName, groupSpecialization, groupCourse);
+        }
+        public void AddStudent(Student student)
+        {
+            students.Add(student);
+        }
+        public Student GetStudent(int index)
+        {
+            if (index >= 0 && index < students.Count)
+                return students[index];
+            else
+            {
+                throw new IndexOutOfRangeException("Invalid index!");
+
+            }
+        }
+        public void PrintGroupInfo()
+        {
+            Console.WriteLine($"Group name: {groupName}");
+            Console.WriteLine($"Group specialization: {groupSpecialization}");
+            Console.WriteLine($"Group course: {groupCourse}");
+            Console.WriteLine("Students:");
+            students.Sort();
+
+            for (int i = 0; i < students.Count; i++) 
+            {
+                Console.WriteLine($"{i+1}. {students[i].ToString()}");
+            }
+
+        }
+        public void EditGroupInfo (string groupName, string groupSpecialization, int groupCourse)
+        {
+            SetGroupInfo(groupName, groupSpecialization, groupCourse);
+        }
+        public void EditStudentInfo(Student student, string adress, string phoneNumber)
+        {
+            student.SetAdress(adress);
+            student.SetPhoneNumber(phoneNumber);
+        }
+        public void RemoveStudent(Student student)
+        {
+            if (!students.Contains(student))
+            {
+                Console.WriteLine("There is no such student in the group!");
+                return;
+            }
+            students.Remove(student);
+        }
+        public void TransferStudent(Group group, Student student)
+        {
+            if (!students.Contains(student))
+            {
+                Console.WriteLine("There is no such student in the group!");
+                return;
+            }
+            if (group.students.Contains(student))
+            {
+                Console.WriteLine("Student is already in this group!");
+                return;
+            }
+            students.Remove(student);
+            group.AddStudent(student);
+        }
+        public void ExpelTheWorstStudent()
+        {
+            Student theWorstStudent = null;
+            double minValue = 12;
+
+            foreach (var student in students)
+            {
+                double totalGrade = student.GetTotalGrade();
+                if (totalGrade < minValue)
+                {
+                    minValue = totalGrade;
+                    theWorstStudent = student;
+                }
+
+
+            }
+            Console.WriteLine($"The worst student is:\n\n{theWorstStudent}\n\nwith grade: {minValue}. This student will be expelled from the group!");
+            students.Remove(theWorstStudent);
+        }
+        public void ExpelLowScorers()
+        {
+            Student lowScorer = null;
+
+            for (int i = 0; i < students.Count; i++)
+            {
+                double totalGrade = students[i].GetTotalGrade();
+                if (totalGrade < 7)
+                {
+                    lowScorer = students[i];
+                    students.Remove(lowScorer);
+                }
+            }
+        }
+        }
+}
---
+++ Student.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw_classes_csharp
+{
+    public class BirthdayDate
+    {
+        private int day;
+        private int month;
+        private int year;
+
+        public BirthdayDate()
+            : this(1, 1, 2000) { }
+
+        public BirthdayDate(int day, int month, int year)
+        {
+            SetBirthdayDate(day, month, year);
+        }
+        public void SetBirthdayDate(int day, int month, int year)
+        {
+            if (day > 0 && day <= 31)
+                this.day = day;
+            else
+            {
+                Console.WriteLine("Invalid value for day!");
+                return;
+            }
+            if (month > 0 && month <= 12)
+                this.month = month;
+            else
+            {
+                Console.WriteLine("Invalid value for month!");
+                return;
+            }
+            if (year > 0 && year <= 2021)
+                this.year = year;
+            else
+            {
+                Console.WriteLine("Invalid value for year!");
+                return;
+            }
+        }
+        public (int day, int month, int year) GetBirthdayDate()
+        {
+            return (day, month, year);
+        }
+    }
+
+        public class FullName
+        {
+            private string? firstName;
+            private string? lastName;
+            private string? patronymic;
+            public FullName()
+                : this("Unknown", "Unknown", "Unknown") { }
+            public FullName(string firstName, string lastName, string patronymic)
+            {
+                SetFullName(firstName, lastName, patronymic);
+            }
+            public void SetFullName(string firstName, string lastName, string patronymic)
+            {
+                if (firstName.Length > 0)
+                    this.firstName = firstName;
+                else
+                {
+                    Console.WriteLine("Invalid value for first name!");
+                    return;
+                }
+                if (lastName.Length > 0)
+                    this.lastName = lastName;
+                else
+                {
+                    Console.WriteLine("Invalid value for last name!");
+                    return;
+                }
+                if (patronymic.Length > 0)
+                    this.patronymic = patronymic;
+                else
+                {
+                    Console.WriteLine("Invalid value for patronymic!");
+                    return;
+                }
+
+            }
+            public (string firstName, string lastName, string patronymic) GetFullName()
+            {
+                return (firstName, lastName, patronymic);
+            }
+
+        }
+    public class Student : IComparable<Student> 
+    {
+        private FullName fullName;
+        private BirthdayDate birthday;
+        private string? address;
+        private string? phoneNumber;
+        private List<int> courseworks;
+        private List<int> credits;
+        private List<int> exams;
+
+        public Student()
+            : this(new FullName(), new BirthdayDate(), "Unknown", "Unknown") { }
+        public Student(FullName fullName, BirthdayDate birthday, string adress, string phoneNumber)
+        {
+            this.fullName = fullName;
+            this.birthday = birthday;
+            SetAdress(adress);
+            SetPhoneNumber(phoneNumber);
+            courseworks = new List<int>();
+            credits = new List<int>();
+            exams = new List<int>();
+        }
+        public int CompareTo(Student other)
+        {
+            return this.fullName.GetFullName().lastName.CompareTo(other.fullName.GetFullName().lastName);
+        }
+        public void SetAdress(string address)
+        {
+            if (address.Length > 0)
+                this.address = address;
+            else
+            {
+                Console.WriteLine("Invalid value for address!");
+                return;
+            }
+        }
+        public string GetAddress()
+        {
+            return address;
+        }
+        public void SetPhoneNumber(string phoneNumber)
+        {
+            if (phoneNumber.Length > 0)
+                this.phoneNumber = phoneNumber;
+            else
+            {
+                Console.WriteLine("Invalid value for phone number!");
+                return;
+            }
+        }
+        public string GetPhoneNumber()
+        {
+            return phoneNumber;
+        }
+        public void AddCourseworkGrade(int courseworkGrade)
+        {
+            if (courseworkGrade > 0)
+                courseworks.Add(courseworkGrade);
+            else
+            {
+                Console.WriteLine("Invalid value for coursework!");
+                return;
+            }
+        }
+        public int GetCourseworkGrade(int index)
+        {
+            if (index >= 0 && index < courseworks.Count)
+            {
+                Console.WriteLine("Invalid index!");
+                return -1;
+            }
+            return courseworks[index];
+        }
+        public void AddCreditGrade(int creditGrade)
+        {
+            if (creditGrade > 0)
+                credits.Add(creditGrade);
+            else
+            {
+                Console.WriteLine("Invalid value for credit!");
+                return;
+            }
+        }
+        public int GetCreditGrade(int index)
+        {
+            if (index >= 0 && index < credits.Count)
+            {
+                Console.WriteLine("Invalid index!");
+                return -1;
+            }
+            return credits[index];
+        }
+        public void AddExamGrade(int examGrade)
+        {
+            if (examGrade > 0)
+                exams.Add(examGrade);
+            else
+            {
+                Console.WriteLine("Invalid value for exam!");
+                return;
+            }
+        }
+        public int GetExamGrade(int index)
+        {
+            if (index >= 0 && index < exams.Count)
+            {
+                Console.WriteLine("Invalid index!");
+                return -1;
+            }
+            return exams[index];
+        }
+        public double GetTotalGrade()
+        {
+            var courseworksAvg = courseworks.Average();
+            var creditsAvg = credits.Average();
+            var examsAvg = exams.Average();
+            return (courseworksAvg + creditsAvg + examsAvg)/3;
+        }
+        public void PrintStudentInfo()
+        {
+            var (firstName, lastName, patronymic) = fullName.GetFullName();
+            var (day, month, year) = birthday.GetBirthdayDate();
+            Console.WriteLine($"Full name: {lastName} {firstName} {patronymic}");
+            Console.WriteLine($"Birthday date: {day}.{month}.{year}");
+            Console.WriteLine($"Address: {address}");
+            Console.WriteLine($"Phone number: {phoneNumber}");
+            
+            if (courseworks.Count == 0)
+            {
+                Console.WriteLine("No coursework grades!");
+                return;
+            }
+            else {
+                Console.WriteLine("Grades for courseworks:");
+                foreach (var coursework in courseworks)
+                {
+
+                    Console.WriteLine(coursework);
+                }
+            }
+            if (credits.Count == 0)
+            {
+                Console.WriteLine("No credit grades!");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Grades for credits:");
+                foreach (var credit in credits)
+                {
+                    Console.WriteLine(credit);
+                }
+            }
+            
+            if (exams.Count == 0)
+            {
+                Console.WriteLine("No exam grades!");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Grades for exams:");
+                foreach (var exam in exams)
+                {
+                    Console.WriteLine(exam);
+                }
+            }
+        }
+        public override string ToString()
+        {
+            var (firstName, lastName, patronymic) = fullName.GetFullName();
+            var (day, month, year) = birthday.GetBirthdayDate();
+            return $"Full name: {lastName} {firstName} {patronymic}\nDate of birthday: {day}.{month}.{year}\nAddress: {address}\nPhone number: {phoneNumber}";
+        }
+    }
+}
+    
