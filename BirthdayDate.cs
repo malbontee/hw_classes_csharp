402d8301d8d5022d96ce7a73068ac3cdcb8a438c
malbontee <144820547+malbontee@users.noreply.github.com>
2025-01-28 19:50:25 +0200

    + interface ICloneable

---
+++ BirthdayDate.cs
@@ -0,0 +1,70 @@
+﻿namespace hw_classes_csharp
+{
+    public class BirthdayDate
+    {
+        private int day;
+        private int month;
+        private int year;
+
+        public BirthdayDate()
+            : this(1, 1, 2000) { }
+
+        public BirthdayDate(int day, int month, int year)
+        {
+            SetBirthdayDate(day, month, year);
+        }
+        public int Day
+        {
+            get { return day; }
+            set
+            {
+                if (value > 0 && value <= 31)
+                    day = value;
+                else
+                    throw new ArgumentOutOfRangeException("Invalid value for day!");
+            }
+        }
+        public int Month
+        {
+            get { return month; }
+            set
+            {
+                if (value > 0 && value <= 12)
+                    month = value;
+                else
+                    throw new ArgumentOutOfRangeException("Invalid value for month!");
+            }
+        }
+        public int Year
+        {
+            get { return year; }
+            set
+            {
+                if (value > 0 && value <= 2021)
+                    year = value;
+                else
+                    throw new ArgumentOutOfRangeException("Invalid value for year!");
+            }
+        }
+        public void SetBirthdayDate(int day, int month, int year)
+        {
+            if (day > 0 && day <= 31)
+                this.day = day;
+            else
+                throw new ArgumentOutOfRangeException("Invalid value for day!");
+            if (month > 0 && month <= 12)
+                this.month = month;
+            else
+                throw new ArgumentOutOfRangeException("Invalid value for month!");
+            if (year > 0 && year <= 2021)
+                this.year = year;
+            else
+                throw new ArgumentOutOfRangeException("Invalid value for year!");
+        }
+        public (int day, int month, int year) GetBirthdayDate()
+        {
+            return (day, month, year);
+        }
+    }
+}
+    
---
+++ FullName.cs
@@ -0,0 +1,71 @@
+﻿namespace hw_classes_csharp
+{
+    public class FullName
+        {
+            private string? firstName;
+            private string? lastName;
+            private string? patronymic;
+            public FullName()
+                : this("Unknown", "Unknown", "Unknown") { }
+            public FullName(string firstName, string lastName, string patronymic)
+            {
+                SetFullName(firstName, lastName, patronymic);
+            }
+        public string? FirstName
+        {
+            get { return firstName; }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                    firstName = value;
+                else
+                    throw new ArgumentException("First name cannot be null or empty!");
+            }
+        }
+        public string? LastName
+        {
+            get { return lastName; }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                    lastName = value;
+                else
+                    throw new ArgumentException("Last name cannot be null or empty!");
+            }
+        }
+        public string? Patronymic
+        {
+            get { return patronymic; }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                    patronymic = value;
+                else
+                    throw new ArgumentException("Patronymic cannot be null or empty!");
+            }
+        }
+        public void SetFullName(string firstName, string lastName, string patronymic)
+            {
+                if (!string.IsNullOrEmpty(firstName))
+                    this.firstName = firstName;
+                else
+                    throw new ArgumentException("First name cannot be null or empty!"); 
+                if (!string.IsNullOrEmpty(lastName))
+                    this.lastName = lastName;
+                else
+                    throw new ArgumentException("Last name cannot be null or empty!"); 
+
+                if (!string.IsNullOrEmpty(patronymic))
+                    this.patronymic = patronymic;
+                else
+                    throw new ArgumentException("Patronymic cannot be null or empty!");
+
+            }
+            public (string? firstName, string? lastName, string? patronymic) GetFullName()
+            {
+                return (firstName, lastName, patronymic);
+            }
+
+        }
+}
+    
